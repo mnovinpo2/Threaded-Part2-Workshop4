b56b5027e1ec93d210ae69a39d5854faeddc1693
Moose <mustafa.hersi@edu.sait.ca>
2024-01-23 12:57:33 -0700

    Add project files.

---
+++ TravelExpertsGUI/frmTravelExperts.cs
@@ -0,0 +1,17 @@
+using TravelExpertsData;
+
+namespace TravelExpertsGUI
+{
+    public partial class frmTravelExperts : Form
+    {
+        public frmTravelExperts()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
